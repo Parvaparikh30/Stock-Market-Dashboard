7add6639a944c13e0d3191447517fd99fe3d4c0c
Parvaparikh30 <58827015+Parvaparikh30@users.noreply.github.com>
2021-08-17 10:40:10 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+
+#Enter the API Details from Twitter API Website
+consumer_key=''
+consumer_secret= ''
+access_token = ''
+access_token_secret= ''
+
+
+TWITTER_USERNAMES = [
+        "YahooFinance",
+            "CNBC",
+                "moneycontrolcom",
+    "livemint",
+    "EconomicTimes",
+
+    'Reutersindia',
+    "FinancialTimes",
+    "NDTVProfit",
+    "WIONews"
+
+]
